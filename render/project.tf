2cc282945f625ca4cb0476d249c81b904edc93ad
Zaschaff <124943183+Zaschaff@users.noreply.github.com>
2025-04-18 12:48:26 -0400

    a

--- render/project.tf
+++ render/project.tf
@@ -1,1 +1,13 @@
- 
+resource "render_project" "p666" {
+  name = "P666 Spring 2025"
+  environments = {
+    "development" = {
+      name            = "development"
+      protected_status = "unprotected"
+    },
+    "staging" = {
+      name            = "staging"
+      protected_status = "protected"
+    }
+  }
+} 
